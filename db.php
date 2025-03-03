464e391d52a51586a6813a54eb137a11718f8813
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-03 15:14:48 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,18 @@
 <?php
 // Database connection details
-$host = 'rmcdatabaseserver.mysql.database.azure.com';
+$host = 'rmcdatabaseserver.database.windows.net';
 $dbname = 'rmc';
 $username = 'ibrahimbabangida50';
 $password = '@Babrahim50';
 
 // Path to the SSL certificate
 $ssl_cert = __DIR__ . '/RMCF/BaltimoreCyberTrustRoot.crt.pem';
-
 try {
     // Enable SSL in the connection options
     $options = [
         PDO::MYSQL_ATTR_SSL_CA => $ssl_cert,
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Disable server certificate verification
     ];
-
     // Create a PDO instance with SSL
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
