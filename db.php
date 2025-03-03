7e30b25cb8a4625df21235578d1be9de60d4a30a
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-03 14:24:45 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection details
-$host = 'rmcdatabaseserver.database.windows.net.mysql.database.azure.com';
+$host = 'rmcdatabaseserver.azure.com';
 $dbname = 'rmc';
 $username = 'ibrahimbabangida50';
 $password = '@Babrahim50';
