81e65a5bdb3767ffc201abf5a19deabd8b6832e6
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-05 17:30:53 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,25 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:rmcdatabaseserver.database.windows.net,1433; Database = salama", "ibrahimbabangida50", "{@Babrahim50}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ibrahimbabangida50", "pwd" => "{@Babrahim50}", "Database" => "salama", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// Database connection details
 $serverName = "tcp:rmcdatabaseserver.database.windows.net,1433";
+$databaseName = "salama";
+$username = "ibrahimbabangida50";
+$password = "@Babrahim50";
+
+// SQL Server Extension Connection
+$connectionInfo = array(
+    "UID" => $username,
+    "PWD" => $password,
+    "Database" => $databaseName,
+    "LoginTimeout" => 30,
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0
+);
+
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    echo "Connected successfully using sqlsrv_connect!";
+}
 ?>
