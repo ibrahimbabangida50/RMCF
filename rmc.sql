9b7faefa96f0662582c62390f8ebe87d398821f1
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-05 23:08:03 +0200

    Update rmc.sql

--- rmc.sql
+++ rmc.sql
@@ -1,6 +1,6 @@
--- Create Users table
-CREATE TABLE IF NOT EXISTS Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- Create the Users table
+CREATE TABLE Users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
     phone VARCHAR(15) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     registration_type VARCHAR(50) NOT NULL,
@@ -9,29 +9,32 @@ CREATE TABLE IF NOT EXISTS Users (
     district VARCHAR(100) NOT NULL
 );
 
--- Create Contributions table
-CREATE TABLE IF NOT EXISTS Contributions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- Create the Contributions table
+CREATE TABLE Contributions (
+    id INT IDENTITY(1,1) PRIMARY KEY,
     phone VARCHAR(15) NOT NULL,
     amount INT NOT NULL,
     payment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (phone) REFERENCES Users(phone)
 );
+
+-- Create the Sadaqh table
 CREATE TABLE Sadaqh (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     donor_name VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
     district VARCHAR(100),
     amount DECIMAL(10,2) NOT NULL,
-    donation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    donation_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create the Zakat table
 CREATE TABLE Zakat (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     donor_name VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
     district VARCHAR(100),
     Zakat_type VARCHAR(100),
     amount DECIMAL(10,2) NOT NULL,
-    donation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    donation_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
