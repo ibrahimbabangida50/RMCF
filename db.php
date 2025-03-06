f148e773f7c5e0223d5341750acb7241284c4987
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 15:32:03 +0200

    Update db.php

--- db.php
+++ db.php
@@ -3,9 +3,8 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:babangida.database.windows.net,1433; Database = baba", "ibrahimbabangida50", "@Babrahim50");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage(); // More specific error message
+     // More specific error message
     die();
 }
 ?>
