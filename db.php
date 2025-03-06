303cf61dd7f20a61bf3aad890d1f389312975110
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 10:37:22 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:babangida.database.windows.net,1433; Database = baba", "ibrahimbabangida50", "@Babrahim50");
+    $conn = new PDO("sqlsrv:server = tcp:babangida.database.windows.net,1433; Database = baba", "ibrahimbabangida50","@Babrahim50");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
