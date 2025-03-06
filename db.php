c65ddb3d1544b018cb6727049a44492518729af2
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 07:49:22 +0200

    Update db.php

--- db.php
+++ db.php
@@ -13,5 +13,4 @@ catch (PDOException $e) {
 $connectionInfo = array("UID" => "ibrahimbabangida50", "pwd" => "{@Babrahim50}", "Database" => "baba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:babangida.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
 ?>
