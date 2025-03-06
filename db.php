453bb9a4f280fd2bb74f2f5b47844239173a6ca9
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 16:03:01 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,11 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:babangida.database.windows.net,1433; Database = baba", "ibrahimbabangida50", "@Babrahim50");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-
-    // Test query
-    $sql = "SELECT 1 AS test";
-    $stmt = $conn->query($sql);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die();
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 ?>
