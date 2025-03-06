64a09786e1fa8a729467097f0df4164fc9346810
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 11:03:24 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:babangida.database.windows.net,1433; Database = baba", "ibrahimbabangida50", "@Babrahim50");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ibrahimbabangida50", "pwd" => "@Babrahim50", "Database" => "baba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:babangida.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 }
 ?>
