6409a454c0ce6978aac3a015471af65ea9e65555
ibrahimbabangida50 <ibrahimbabangida50@gmail.com>
2025-03-06 10:32:15 +0200

    Update db.php

--- db.php
+++ db.php
@@ -8,9 +8,4 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ibrahimbabangida50", "pwd" => "@Babrahim50", "Database" => "baba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:babangida.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
